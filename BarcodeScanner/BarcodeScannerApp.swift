4bd58d7161e4abfe2901563d903bf2e7f3c3fbbd
samarakoon-ryan <samarakoon.ryan@gmail.com>
2023-10-23 18:09:32 -0400

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Ryan on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
